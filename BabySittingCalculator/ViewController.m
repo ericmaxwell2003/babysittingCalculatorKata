7f1fc0fb1a24de27a2aa47c194a88f5d0c5e9fc1
Eric Maxwell <ericmaxwell2003@yahoo.com>
2015-03-15 03:06:13 -0400

    Added a simple, quickly thrown together UI overtop of the calculator.  I didn't spend much time doing anything fancy, since the point of this kata is to illustrate TDD.

--- BabySittingCalculator/ViewController.m
+++ BabySittingCalculator/ViewController.m
@@ -7,8 +7,25 @@
 //
 
 #import "ViewController.h"
+#import "Calculator.h"
+
+#define AM_SEGMENT 0
+#define PM_SEGMENT 1
 
 @interface ViewController ()
+
+@property (nonatomic, strong) Calculator *calculator;
+
+@property (weak, nonatomic) IBOutlet UILabel *payAmount;
+@property (weak, nonatomic) IBOutlet UILabel *errorLabel;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *startAmPmControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *endAmPmControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *bedtimeAmPmControl;
+
+@property (weak, nonatomic) IBOutlet UITextField *startTimeText;
+@property (weak, nonatomic) IBOutlet UITextField *endTimeText;
+@property (weak, nonatomic) IBOutlet UITextField *bedtimeText;
 
 @end
 
@@ -16,12 +33,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.errorLabel.hidden = YES;
+    self.calculator = [[Calculator alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)calculatePayment:(id)sender {
+
+    @try {
+        NSString *start = self.startTimeText.text;
+        BOOL isStartPm = self.startAmPmControl.selectedSegmentIndex == PM_SEGMENT;
+        int startHour = isStartPm ? ([start intValue] + 12) : [start intValue];
+
+        NSString *end = self.endTimeText.text;
+        BOOL isEndPm = self.endAmPmControl.selectedSegmentIndex == PM_SEGMENT;
+        int endHour = isEndPm ? ([end intValue] + 12) : [end intValue];
+
+        NSString *bedtime = self.bedtimeText.text;
+        BOOL isBedtimePm = self.bedtimeAmPmControl.selectedSegmentIndex == PM_SEGMENT;
+        int bedTimeHour = isBedtimePm ? ([bedtime intValue] + 12) : [bedtime intValue];
+        
+        double result = [self.calculator calculateOneNightPayFromStartHour:startHour toEndHour:endHour withBedTimeAt:bedTimeHour];
+        self.payAmount.text = [NSString stringWithFormat:@"$%d", (int)result];
+        self.errorLabel.hidden = YES;
+    }
+    @catch (NSException *exception) {
+        self.payAmount.text = @"$0";
+        self.errorLabel.hidden = NO;
+    }
+    
+    
+    
+    
+}
+
 
 @end
